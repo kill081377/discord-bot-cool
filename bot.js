a9b2f1e8ace9a732ed8b31c0f6a7c5980cd9cedd
kill081377 <lupomano@gmail.com>
2018-02-16 10:47:31 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,70 +1,39 @@
-import discord
-from discord.ext.commands import Bot
-from discord.ext import commands
-import asyncio
-import time
+onst Discord = require("discord.js");
+const client = new Discord.Client();
+const Prefix = "/"
 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
-Client = discord.Client()
-client = commands.Bot(command_prefix = "!")
+client.on('message', msg => {
+  if (msg.content === Prefix + 'Creator') {
+    msg.reply('My creator is LimitedMine(Adam) C:');
+  }
+});
 
+client.on('message', msg => {
+  if (msg.content === Prefix + 'info') {
+    msg.reply('Hello im PreNetwork Bot! I was Made By PreNetwork xD C:');
+  }
+});
 
-@client.event
-async def on_ready():
-    print("Bot is online and connected to Discord")
+client.on('message', msg => {
+  if (msg.content === Prefix + 'Commands') {
+    msg.reply('Commands are /Creator /info Thats all :( Sorry)');
+  }
+});
 
+client.on('message', msg => {
+  if (msg.content === Prefix + 'commands') {
+    msg.reply('Commands are /Creator /info Thats all :( Sorry)');
+  }
+});
 
-@client.event
-async def on_message(message):
-    if message.content.upper().startswith('!BING'):
-        userID = message.author.id
-        await client.send_message(message.channel, "<@%s> Bong!" % (userID))
-    if message.content.upper().startswith('!SAY'):
-        args = message.content.split(" ")
-        #args[0] = !SAY
-        #args[1] = Hey
-        #args[2] = There
-        #args[1:] = Hey There
-        await client.send_message(message.channel, "%s" % (" ".join(args[1:])))
+client.on('message', msg => {
+  if (msg.content === Prefix + 'Test') {
+    msg.reply('Marcus is a creep!');
+  }
+});
 
-
-@client.event
-async def on_message(message):
-    if message.content.upper().startswith('!PING'):
-        userID = message.author.id
-        await client.send_message(message.channel, "<@%s> Pong!" % (userID))
-    if message.content.upper().startswith('!SAY'):
-        if message.author.id == "<user id>": #Replace <User ID> with the ID of the user you want to be able to execute this command!
-            args = message.content.split(" ")
-            await client.send_message(message.channel, "%s" % (" ".join(args[1:])))
-        else:
-            await client.send_message(message.channel, "You do not have permission")
-    if message.content.upper().startswith('!AMIADMIN'):
-        if "413969692953411587" in [role.id for role in message.author.roles]: #Replace <Role ID> with the ID of the role you want to be able to execute this command
-            await client.send_message(message.channel, "You are an admin")
-        else:
-            await client.send_message(message.channel, "You are not an admin")
-
-
-chat_filter = ["FUCK", "CUNT", "ISIS", "SHIT", "HOE", "HORE", "CUC", "SUC", "SU3"]
-bypass_list = []
-
-@client.event
-async def on_ready():
-    print("Bot is online and connected to Discord")
-
-@client.event
-async def on_message(message):
-    userID = message.author.id
-    contents = message.content.split(" ") #contents is a list type
-    for word in contents:
-        if word.upper() in chat_filter:
-            if not message.author.id in bypass_list:
-                try:
-                    await client.delete_message(message)
-                    await client.send_message(message.channel, "**Hey!** Don't curse!")
-                except discord.errors.NotFound:
-                    return        
-
-
-client.run("NDEzOTU2Mzc0NDAyMzAxOTUy.DWggXg._P8NoJxWk_Z9GGNuaopN3fRb3vU")
+client.login('MzY0NDA2OTgyMjM4NTM1Njgw.DLP0gQ.4_sB1QozuVSNh0k03VP8blkJoGU');
