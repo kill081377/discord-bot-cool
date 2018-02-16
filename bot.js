0ce6d923e1a2cea69f9a65beb4adc0d8d326d46c
kill081377 <lupomano@gmail.com>
2018-02-16 14:33:23 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,39 +1,16 @@
 onst Discord = require("discord.js");
 const client = new Discord.Client();
-const Prefix = "/"
+const Prefix = "t."
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === Prefix + 'Creator') {
-    msg.reply('My creator is LimitedMine(Adam) C:');
+  if (msg.content === Prefix + 'fangroupsimulator') {
+    msg.reply('Go to #script-release' ${client.user.tag}!);
   }
 });
 
-client.on('message', msg => {
-  if (msg.content === Prefix + 'info') {
-    msg.reply('Hello im PreNetwork Bot! I was Made By PreNetwork xD C:');
-  }
-});
 
-client.on('message', msg => {
-  if (msg.content === Prefix + 'Commands') {
-    msg.reply('Commands are /Creator /info Thats all :( Sorry)');
-  }
-});
-
-client.on('message', msg => {
-  if (msg.content === Prefix + 'commands') {
-    msg.reply('Commands are /Creator /info Thats all :( Sorry)');
-  }
-});
-
-client.on('message', msg => {
-  if (msg.content === Prefix + 'Test') {
-    msg.reply('Marcus is a creep!');
-  }
-});
-
-client.login('MzY0NDA2OTgyMjM4NTM1Njgw.DLP0gQ.4_sB1QozuVSNh0k03VP8blkJoGU');
+client.login('NDEzOTU2Mzc0NDAyMzAxOTUy.DWhuRA.yEI6iqqiT9LWlfryfTGGE94h0Fo');
